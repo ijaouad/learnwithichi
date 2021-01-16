f84395b2fb7bddd4df5cb3042444bdefb92e09ec
JAOUAD ICHI <71087285+jaouadichi@users.noreply.github.com>
2021-01-16 21:16:31 +0100

    Add initial ,Learn with ICHI site, files

---
+++ script.js
@@ -0,0 +1,10 @@
+const OpenTab = (tab) => {
+
+    let tabs = document.getElementsByClassName("tab");
+
+    for (let i = 0; i < tabs.length; i++) {
+        tabs[i].style.display = 'none';
+    }
+
+    document.getElementById(tab).style.display = 'block';
+}
