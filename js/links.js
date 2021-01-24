5a23721814bafaca15875215836ac0398ab8ba12
JAOUAD ICHI <71087285+jaouadichi@users.noreply.github.com>
2021-01-24 16:43:37 +0100

    Update links.js

--- js/links.js
+++ js/links.js
@@ -14,6 +14,14 @@ const homeLinks = [
     {
         text:"Home : Lorem, ipsum dolor sit amet consectetur adipisicing.",
         link:"#"
+    },
+    {
+        text:"Home : Lorem, ipsum dolor sit amet consectetur adipisicing.",
+        link:"#"
+    },
+    {
+        text:"Home : Lorem, ipsum dolor sit amet consectetur adipisicing.",
+        link:"#"
     }
 ];
 
@@ -35,11 +43,27 @@ const freeLinks = [
     {
         text:"Free : Lorem, ipsum dolor sit amet consectetur adipisicing.",
         link:"#"
+    },
+    {
+        text:"Free : Lorem, ipsum dolor sit amet consectetur adipisicing.",
+        link:"#"
+    },
+    {
+        text:"Free : Lorem, ipsum dolor sit amet consectetur adipisicing.",
+        link:"#"
     }
 ]
 
 
 const tipsLinks = [
+    {
+        text:"Tips : Lorem, ipsum dolor sit amet consectetur adipisicing.",
+        link:"#"
+    },
+    {
+        text:"Tips : Lorem, ipsum dolor sit amet consectetur adipisicing.",
+        link:"#"
+    },
     {
         text:"Tips : Lorem, ipsum dolor sit amet consectetur adipisicing.",
         link:"#"
