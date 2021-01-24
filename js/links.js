a513f8abdacc728cba4de9989e038830c878f94e
JAOUAD ICHI <71087285+jaouadichi@users.noreply.github.com>
2021-01-24 16:33:42 +0100

    Add files via upload

---
+++ js/links.js
@@ -0,0 +1,59 @@
+const homeLinks = [
+    {
+        text:"Home : Lorem, ipsum dolor sit amet consectetur adipisicing.",
+        link:"#"
+    },
+    {
+        text:"Home : Lorem, ipsum dolor sit amet consectetur adipisicing.",
+        link:"#"
+    },
+    {
+        text:"Home : Lorem, ipsum dolor sit amet consectetur adipisicing.",
+        link:"#"
+    },
+    {
+        text:"Home : Lorem, ipsum dolor sit amet consectetur adipisicing.",
+        link:"#"
+    }
+];
+
+
+
+const freeLinks = [
+    {
+        text:"Free : Lorem, ipsum dolor sit amet consectetur adipisicing.",
+        link:"#"
+    },
+    {
+        text:"Free : Lorem, ipsum dolor sit amet consectetur adipisicing.",
+        link:"#"
+    },
+    {
+        text:"Free : Lorem, ipsum dolor sit amet consectetur adipisicing.",
+        link:"#"
+    },
+    {
+        text:"Free : Lorem, ipsum dolor sit amet consectetur adipisicing.",
+        link:"#"
+    }
+]
+
+
+const tipsLinks = [
+    {
+        text:"Tips : Lorem, ipsum dolor sit amet consectetur adipisicing.",
+        link:"#"
+    },
+    {
+        text:"Tips : Lorem, ipsum dolor sit amet consectetur adipisicing.",
+        link:"#"
+    },
+    {
+        text:"Tips : Lorem, ipsum dolor sit amet consectetur adipisicing.",
+        link:"#"
+    },
+    {
+        text:"Tips : Lorem, ipsum dolor sit amet consectetur adipisicing.",
+        link:"#"
+    }
+]
